f6e57d959bfec42e336e993819aa724ea504d85d
Daniel Ogenrwot <danielogen@gmail.com>
2024-08-19 15:31:05 -0700

    Update test_counter.py

--- tests/test_counter.py
+++ tests/test_counter.py
@@ -10,9 +10,27 @@ how to call the web service and assert what it should return.
 - The service must be able to update a counter by name.
 - The service must be able to read the counter
 """
-
 import pytest
-
 from src.counter import app
-
 from src import status
+
+@pytest.fixture
+def client():
+    """Fixture to create a test client."""
+    return app.test_client()
+
+@pytest.mark.usefixtures("client")
+class TestCounterEndpoints:
+    """Test cases for Counter-related endpoints"""
+    
+    def test_create_a_counter(self, client):
+        """It should create a counter"""
+        result = client.post('/counters/foo')
+        assert result.status_code == status.HTTP_201_CREATED
+    
+    def test_duplicate_a_counter(self, client):
+        """It should return an error for duplicates"""
+        result = client.post('/counters/bar')
+        assert result.status_code == status.HTTP_201_CREATED
+        result = client.post('/counters/bar')
+        assert result.status_code == status.HTTP_409_CONFLICT
